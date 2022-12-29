7dd8c6a823e8c3e9b9c31dad6efc88b2ad85d37c
Nithin Chintala <>
2022-12-29 11:43:23 -0500

    remove leaderboard logic

--- test.py
+++ test.py
@@ -3,8 +3,6 @@
 import getpass, os, argparse, atexit
 from run import Simulation
 
-LEADERBOARD_OUTPUT = '/course/cs3700f20/stats/project6/'
-
 # Constants for tuning the difficulty of the tests
 PACKETS_LOW = 500.0
 PACKETS_MID = 800.0
@@ -88,12 +86,10 @@ trials.append(run_test('partition-2.json', 'No drops, 2 easy partitions, 20% rea
 trials.append(run_test('partition-3.json', 'No drops, 1 hard partition, 20% read'))
 trials.append(run_test('partition-4.json', 'No drops, 2 hard partitions, 20% read'))
 
-ldr = open(LEADERBOARD_OUTPUT + getpass.getuser(), 'w')
-
 print 'Advanced tests:'
-trials.append(run_test('advanced-1.json', '10% drops, 2 hard partitions and 1 leader failures, 20% read', ldr))
-trials.append(run_test('advanced-2.json', '15% drops, 2 leader failures, 20% read', ldr))
-trials.append(run_test('advanced-3.json', '50% drops, 1 leader failure, 20% read', ldr))
-trials.append(run_test('advanced-4.json', '10% drops, 3 hard partions and 1 leader kill, 20% read', ldr))
+trials.append(run_test('advanced-1.json', '10% drops, 2 hard partitions and 1 leader failures, 20% read'))
+trials.append(run_test('advanced-2.json', '15% drops, 2 leader failures, 20% read'))
+trials.append(run_test('advanced-3.json', '50% drops, 1 leader failure, 20% read'))
+trials.append(run_test('advanced-4.json', '10% drops, 3 hard partions and 1 leader kill, 20% read'))
 
 print 'Passed', len(filter(None, trials)), 'out of', len(trials), 'tests'
